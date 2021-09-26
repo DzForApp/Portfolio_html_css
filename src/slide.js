80ed22662d68cc29786969031e612e4e27b0fc0c
dzforapp <dzforapp@gmail.com>
2021-09-26 16:59:37 +0100

    new file:   src/imgsvg/facebook.svg 	new file:   src/imgsvg/facebook1.svg 	modified:   src/imgsvg/github.svg 	new file:   src/imgsvg/instagram.svg 	new file:   src/imgsvg/linkedin.svg 	new file:   src/imgsvg/switch (1).svg 	new file:   src/imgsvg/switch (2).svg 	new file:   src/imgsvg/wave.svg 	new file:   src/imgsvg/wave2.svg 	modified:   src/index1.html 	modified:   src/slide.js

--- src/slide.js
+++ src/slide.js
@@ -1,5 +1,5 @@
-var slidePosition = 1;
-SlideShow(slidePosition);
+var slidePosition =0;
+SlideShow(slidePosition + 1);
 
 // forward/Back controls
 function plusSlides(n) {
@@ -14,15 +14,24 @@ function currentSlide(n) {
 function SlideShow(n) {
   var i;
   var slides = document.getElementsByClassName("Containers");
+   
   var circles = document.getElementsByClassName("dote");
   if (n > slides.length) {slidePosition = 1}
-  if (n < 1) {slidePosition = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
+  if (n < 1) {slidePosition = slides.length+1}
+     
+     for (i = 0; i < slides.length; i++) {
+           
+            slides[i].style.display = "none";
+           
+     }
+     
+  
   for (i = 0; i < circles.length; i++) {
       circles[i].className = circles[i].className.replace(" enable", "");
   }
   slides[slidePosition-1].style.display = "block";
-  circles[slidePosition-1].className += " enable";
+  circles[slidePosition-1].className += " active";
+  setTimeout(showSlides, 2000);
 } 
+
+
